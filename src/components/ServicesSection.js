593edc481c8a1969087e43997590b2e12675bdba
Juanan Fernandez <juanan.fernandez@gmail.com>
2020-12-07 13:42:56 +0100

    Services Section

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,52 @@
+import React from 'react';
+//icons
+import clock from '../img/clock.svg';
+import diaphragm from '../img/diaphragm.svg';
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png';
+
+const ServicesSection = () => {
+   return (
+      <div className="services">
+         <div className="description">
+            <h2>High <span>quality</span> services</h2>
+            <div className="cards"> 
+               <div className="card">
+                  <div className="icon">
+                     <img src={clock} alt="clock"/>
+                     <h3>Efficient</h3>
+                  </div>
+                  <p>Lorem Ipsum dolor sit amet.</p>
+               </div>
+               <div className="card">
+                  <div className="icon">
+                     <img src={diaphragm} alt="diaphragm"/>
+                     <h3>Diaphragm</h3>
+                  </div>
+                  <p>Lorem Ipsum dolor sit amet.</p>
+               </div>
+               <div className="card">
+                  <div className="icon">
+                     <img src={teamwork} alt="team work"/>
+                     <h3>Team Work</h3>
+                  </div>
+                  <p>Lorem Ipsum dolor sit amet.</p>
+               </div>
+               <div className="card">
+                  <div className="icon">
+                     <img src={money} alt="Money"/>
+                     <h3>Money</h3>
+                  </div>
+                  <p>Lorem Ipsum dolor sit amet.</p>
+               </div>
+            </div>
+         </div>
+         <div className="image">
+            <img src={home2} alt="Home2"/>
+         </div>
+      </div>
+   )
+}
+
+export default ServicesSection
